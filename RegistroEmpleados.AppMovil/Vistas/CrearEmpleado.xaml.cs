90c0cbe87f00328a47389612f565a8aebecdb5fd
MICHAEL ALEXIS ARJEL MAYEROVICH <michael.arjel@inacapmail.cl>
2024-11-26 21:18:04 -0300

    Se listan los cargos en el datepicker correspondiente

--- RegistroEmpleados.AppMovil/Vistas/CrearEmpleado.xaml.cs
+++ RegistroEmpleados.AppMovil/Vistas/CrearEmpleado.xaml.cs
@@ -1,11 +1,27 @@
+using Firebase.Database;
+using Firebase.Database.Query;
+using RegistroEmpleados.Modelos.Modelos;
+
 namespace RegistroEmpleados.AppMovil.Vistas;
 
 public partial class CrearEmpleado : ContentPage
 {
-	public CrearEmpleado()
+    FirebaseClient client = new FirebaseClient("https://registroempleados-d7b5e-default-rtdb.firebaseio.com/");
+
+    public List<Cargo> Cargos {  get; set; }
+
+    public CrearEmpleado()
 	{
 		InitializeComponent();
+        ListarCargos();
+        BindingContext = this;
 	}
+
+    private void ListarCargos()
+    {
+        var cargos = client.Child("Cargos").OnceAsync<Cargo>();
+        Cargos=cargos.Result.Select(x=>x.Object).ToList();
+    }
 
     private void guardarButton_Clicked(object sender, EventArgs e)
     {
