c3cbe0c0fd1421178b47c8109a9a3126afb27708
MICHAEL ALEXIS ARJEL MAYEROVICH <michael.arjel@inacapmail.cl>
2024-11-26 23:25:41 -0300

    Se crea primer empleado

--- RegistroEmpleados.AppMovil/Vistas/CrearEmpleado.xaml.cs
+++ RegistroEmpleados.AppMovil/Vistas/CrearEmpleado.xaml.cs
@@ -23,8 +23,30 @@ public partial class CrearEmpleado : ContentPage
         Cargos=cargos.Result.Select(x=>x.Object).ToList();
     }
 
-    private void guardarButton_Clicked(object sender, EventArgs e)
+    private async void guardarButton_Clicked(object sender, EventArgs e)
     {
+        Cargo cargo = cargoPicker.SelectedItem as Cargo;
+
+        var empleado = new Empleado
+        {
+            PrimerNombre = primerNombreEntry.Text,
+            SegundoNombre = segundoNombreEntry.Text,
+            PrimerApellido = primerApellidoEntry.Text,
+            SegundoApellido = segundoApellidoEntry.Text,
+            CorreoElectronico = correoEntry.Text,
+            FechaInicio = fechaInicioPicker.Date,
+            Sueldo = int.Parse(sueldoEntry.Text),
+            Cargo = cargo
+        };
+
+        await client.Child("Empleados").PostAsync(empleado);
+
+        await DisplayAlert("Éxito", $"El empleado {empleado.PrimerNombre} {empleado.PrimerApellido} fue guardado correctamente.", "OK");
+
+        await Navigation.PopAsync();
 
     }
 }
+
+
+   
