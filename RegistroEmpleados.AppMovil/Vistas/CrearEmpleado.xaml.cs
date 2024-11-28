8c88fe44d6a683a8dee243053a3938d4449316db
MICHAEL ALEXIS ARJEL MAYEROVICH <michael.arjel@inacapmail.cl>
2024-11-28 19:37:13 -0300

    Cambios hasta la clase del 27/11

---
+++ RegistroEmpleados.AppMovil/Vistas/CrearEmpleado.xaml.cs
@@ -0,0 +1,14 @@
+namespace RegistroEmpleados.AppMovil.Vistas;
+
+public partial class CrearEmpleado : ContentPage
+{
+	public CrearEmpleado()
+	{
+		InitializeComponent();
+	}
+
+    private void guardarBoton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
