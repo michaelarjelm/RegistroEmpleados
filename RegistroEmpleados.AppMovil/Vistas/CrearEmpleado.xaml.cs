1f5fb79ef098082098a90a2156c3825ff2556cd0
MICHAEL ALEXIS ARJEL MAYEROVICH <michael.arjel@inacapmail.cl>
2024-12-10 20:02:07 -0300

    Se iguala a rama P1

--- RegistroEmpleados.AppMovil/Vistas/CrearEmpleado.xaml.cs
+++ RegistroEmpleados.AppMovil/Vistas/CrearEmpleado.xaml.cs
@@ -1,14 +1,51 @@
+using Firebase.Database;
+using Firebase.Database.Query;
+using RegistroEmpleados.Modelos.Modelos;
+
 namespace RegistroEmpleados.AppMovil.Vistas;
 
 public partial class CrearEmpleado : ContentPage
 {
-	public CrearEmpleado()
+    FirebaseClient client = new FirebaseClient("https://registroempleados-d7b5e-default-rtdb.firebaseio.com/");
+    public List<Cargo> Cargos { get; set; }
+    public CrearEmpleado()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+        ListarCargos();
+        BindingContext = this;
+    }
 
-    private void guardarBoton_Clicked(object sender, EventArgs e)
+    private void ListarCargos()
     {
+        var cargos = client.Child("Cargos").OnceAsync<Cargo>();
+        Cargos = cargos.Result.Select(x => x.Object).ToList();
+    }
 
+    private async void guardarBoton_Clicked(object sender, EventArgs e)
+    {
+        Cargo cargo = cargoPicker.SelectedItem as Cargo;
+
+        var empleado = new Empleado
+        {
+            PrimerNombre = primerNombreEntry.Text,
+            SegundoNombre = segundoNombreEntry.Text,
+            PrimerApellido = primerApellidoEntry.Text,
+            SegundoApellido = segundoApellidoEntry.Text,
+            CorreoElectronico = correoEntry.Text,
+            FechaInicio = fechaInicioPicker.Date,
+            Sueldo = int.Parse(sueldoEntry.Text),
+            Cargo = cargo
+        };
+
+        try
+        {
+            await client.Child("Empleados").PostAsync(empleado);
+            await DisplayAlert("Éxito", $"El empleado {empleado.PrimerNombre} {empleado.PrimerApellido} fue guardado correctamente", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
     }
 }
--- RegistroEmpleados.AppMovil/Vistas/ListarEmpleados.xaml.cs
+++ RegistroEmpleados.AppMovil/Vistas/ListarEmpleados.xaml.cs
@@ -1,15 +1,44 @@
+using Firebase.Database;
+using LiteDB;
+using RegistroEmpleados.Modelos.Modelos;
+using System.Collections.ObjectModel;
+
 namespace RegistroEmpleados.AppMovil.Vistas;
 
 public partial class ListarEmpleados : ContentPage
 {
+    FirebaseClient client = new FirebaseClient("https://registroempleados-d7b5e-default-rtdb.firebaseio.com/");
+    public ObservableCollection<Empleado> Lista { get; set; } = new ObservableCollection<Empleado>();
     public ListarEmpleados()
     {
         InitializeComponent();
+        BindingContext = this;
+        CargarLista();
+    }
+
+    private void CargarLista()
+    {
+        client.Child("Empleados").AsObservable<Empleado>().Subscribe((empleado) =>
+        {
+            if (empleado != null)
+            {
+                Lista.Add(empleado.Object);
+            }
+        });
     }
 
     private void filtroSearchBar_TextChanged(object sender, TextChangedEventArgs e)
     {
+        string filtro = filtroSearchBar.Text.ToLower();
 
+        if (filtro.Length > 0)
+        {
+            listaCollection.ItemsSource = Lista.Where(x => x.NombreCompleto.ToLower().Contains(filtro));
+        }
+        else
+        {
+            listaCollection.ItemsSource = Lista;
+        }
     }
 
     private async void NuevoEmpleadoBoton_Clicked(object sender, EventArgs e)
