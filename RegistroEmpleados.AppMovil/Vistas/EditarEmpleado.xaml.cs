8484e684d7e27109e25ae61b383d6700958e137f
MICHAEL ALEXIS ARJEL MAYEROVICH <michael.arjel@inacapmail.cl>
2024-12-16 00:21:16 -0300

    Se creá lógica para pasar parametros entre vistas y ademas traer empleado seleccionado.

--- RegistroEmpleados.AppMovil/Vistas/EditarEmpleado.xaml.cs
+++ RegistroEmpleados.AppMovil/Vistas/EditarEmpleado.xaml.cs
@@ -1,11 +1,61 @@
+using Firebase.Database;
+using Firebase.Database.Query;
+using RegistroEmpleados.Modelos.Modelos;
+using System.Collections.ObjectModel;
+
 namespace RegistroEmpleados.AppMovil.Vistas;
 
 public partial class EditarEmpleado : ContentPage
 {
-	public EditarEmpleado()
+    FirebaseClient client = new FirebaseClient("https://registroempleados-d7b5e-default-rtdb.firebaseio.com/");
+    public List<Cargo> Cargos { get; set; }
+    public ObservableCollection<string> ListaCargos { get; set; }= new ObservableCollection<string>();
+    private Empleado empleadoActual = new Empleado();
+    private string empleadoId;
+    public EditarEmpleado(string idEmpleado)
 	{
 		InitializeComponent();
-	}
+        BindingContext=this;
+        empleadoId=idEmpleado;
+        CargarListaCargos();
+        CargarEmpleado(empleadoId);
+       
+    }
+
+    private async void CargarListaCargos()
+    {
+        try
+        {
+            var cargos = await client.Child("Cargos").OnceAsync<Cargo>();
+            ListaCargos.Clear();
+            foreach (var cargo in cargos)
+            {
+                ListaCargos.Add(cargo.Object.Nombre);
+            }
+        }
+        catch (Exception ex)
+        {
+
+            await DisplayAlert("Error", "Error:" +ex.Message, "Ok");
+        }
+      
+    }
+
+    private async void CargarEmpleado(string idEmpleado)
+    {
+        var empleado= await client.Child("Empleados").Child(idEmpleado).OnceSingleAsync<Empleado>();
+
+        if (empleado != null)
+        {
+            EditPrimerNombreEntry.Text = empleado.PrimerNombre;
+            EditSegundoNombreEntry.Text = empleado.SegundoNombre;
+            EditPrimerApellidoEntry.Text = empleado.PrimerApellido;
+            EditSegundoApellidoEntry.Text = empleado.SegundoApellido;
+            EditCorreoEntry.Text = empleado.CorreoElectronico;
+            EditSueldoEntry.Text = empleado.Sueldo.ToString();
+            EditCargoPicker.SelectedItem = empleado.Cargo?.Nombre;
+        }
+    }
 
     private void ActualizarButton_Clicked(object sender, EventArgs e)
     {
--- RegistroEmpleados.AppMovil/Vistas/ListarEmpleados.xaml.cs
+++ RegistroEmpleados.AppMovil/Vistas/ListarEmpleados.xaml.cs
@@ -16,15 +16,28 @@ public partial class ListarEmpleados : ContentPage
         CargarLista();
     }
 
-    private void CargarLista()
+    private async void CargarLista()
     {
-        client.Child("Empleados").AsObservable<Empleado>().Subscribe((empleado) =>
+        var empleados = await client.Child("Empleados").OnceAsync<Empleado>();
+
+        var empleadosActivos= empleados.Where(e=>e.Object.Estado==true).ToList();
+
+        foreach (var empleado in empleadosActivos)
         {
-            if (empleado != null)
+            Lista.Add(new Empleado
             {
-                Lista.Add(empleado.Object);
-            }
-        });
+                Id=empleado.Key,
+                PrimerNombre= empleado.Object.PrimerNombre,
+                SegundoNombre= empleado.Object.SegundoNombre,
+                PrimerApellido= empleado.Object.PrimerApellido,
+                SegundoApellido= empleado.Object.SegundoApellido,
+                CorreoElectronico= empleado.Object.CorreoElectronico,
+                Sueldo= empleado.Object.Sueldo,
+                FechaInicio=empleado.Object.FechaInicio,
+                Estado= empleado.Object.Estado,
+                Cargo= empleado.Object.Cargo
+            });
+        }
     }
 
     private void filtroSearchBar_TextChanged(object sender, TextChangedEventArgs e)
@@ -44,5 +57,25 @@ public partial class ListarEmpleados : ContentPage
     private async void NuevoEmpleadoBoton_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new CrearEmpleado());
+    }
+
+    private async void editarButton_Clicked(object sender, EventArgs e)
+    {
+        var boton = sender as ImageButton;
+        var empleado = boton?.CommandParameter as Empleado;
+
+        if (empleado != null &&!string.IsNullOrEmpty(empleado.Id))
+        {
+            await Navigation.PushAsync(new EditarEmpleado(empleado.Id));
+        }
+        else 
+        {
+            await DisplayAlert("Error", "No se pudo obtener la información del empleado", "OK");
+        }
+    }
+
+    private void deshabilitarButton_Clicked(object sender, EventArgs e)
+    {
+
     }
 }
